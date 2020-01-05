41d8c91932d2ab104de44f676d2f1ece9cf64144
M1056314 <M1056314@G1C2ML35469.mindtree.com>
2020-01-05 17:36:33 +0530

    kaingamusic

---
+++ kalingamusiccompany/src/main/java/com/example/DepartmentManagement/KalingamusiccompanyApplication.java
@@ -0,0 +1,13 @@
+package com.example.DepartmentManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalingamusiccompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalingamusiccompanyApplication.class, args);
+	}
+
+}
